7ced6b14d392ff2858572d84d5cb8585562e8ecb
Chris Church <chris@ninemoreminutes.com>
2018-05-31 02:42:13 -0400

    Actually add deregister source file.

---
+++ testProgs/deregisterRTSPStream.cpp
@@ -0,0 +1,96 @@
+/**********
+This library is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation; either version 3 of the License, or (at your
+option) any later version. (See <http://www.gnu.org/copyleft/lesser.html>.)
+
+This library is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this library; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+**********/
+// Copyright (c) 1996-2017, Live Networks, Inc.  All rights reserved
+// A demonstration application that uses our custom RTSP "DEREGISTER" command to deregister a stream
+// with a RTSP client or proxy server main program
+
+#include "liveMedia.hh"
+#include "BasicUsageEnvironment.hh"
+
+char const* programName;
+UsageEnvironment* env;
+
+char const* username = NULL;
+char const* password = NULL;
+
+void deregisterResponseHandler(RTSPClient* rtspClient, int resultCode, char* resultString) {
+  Medium::close(rtspClient);
+
+  // We're done:
+  exit(0);
+}
+
+void usage() {
+  *env << "usage: " << programName << " [-u <username> <password>] "
+    "<remote-client-or-proxy-server-name-or-address> <remote-client-or-proxy-server-port-number> <rtsp-URL-to-register>"
+    " [proxy-URL-suffix]\n";
+  exit(1);
+}
+
+int main(int argc, char const** argv) {
+  // Begin by setting up our usage environment:
+  TaskScheduler* scheduler = BasicTaskScheduler::createNew();
+  env = BasicUsageEnvironment::createNew(*scheduler);
+
+  // Parse command-line options and arguments:
+  // (Unfortunately we can't use getopt() here; Windoze doesn't have it)
+  programName = argv[0];
+  while (argc > 2) {
+    char const* const opt = argv[1];
+    if (opt[0] != '-') break;
+    switch (opt[1]) {
+
+      case 'u': { // specify a username and password
+	if (argc < 4) usage(); // there's no argv[3] (for the "password")
+	username = argv[2];
+	password = argv[3];
+	argv+=2; argc-=2;
+	break;
+      }
+
+      default: {
+	usage();
+	break;
+      }
+    }
+
+    ++argv; --argc;
+  }
+  if (argc != 4 && argc != 5) usage();
+
+  char const* remoteClientNameOrAddress = argv[1];
+
+  portNumBits remoteClientPortNum;
+  if (sscanf(argv[2], "%hu", &remoteClientPortNum) != 1 || remoteClientPortNum == 0 || remoteClientPortNum == 0xFFFF) usage();
+
+  char const* rtspURLToDeregister = argv[3];
+
+  char const* proxyURLSuffix = argc == 4 ? argv[4] : NULL;
+
+  Authenticator* ourAuthenticator = username == NULL ? NULL : new Authenticator(username, password);
+
+  // We have the command-line arguments.  Send the command:
+
+  RTSPDeregisterSender::createNew(*env, remoteClientNameOrAddress, remoteClientPortNum, rtspURLToDeregister,
+				deregisterResponseHandler, ourAuthenticator,
+				proxyURLSuffix,
+				1/*verbosityLevel*/, programName);
+      // Note: This object will be deleted later, by the response handler
+
+  env->taskScheduler().doEventLoop(); // does not return
+
+  return 0; // only to prevent compiler warning
+}
